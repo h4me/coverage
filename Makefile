87beb9b9da19acb63464841bf72d213950c5e172
Pablo <xx8383z@gmail.com>
2019-03-09 16:58:50 +0100

    init

---
+++ Makefile
@@ -0,0 +1,7 @@
+CC=g++-7 -Wall
+
+
+
+main: main.cpp
+	$(CC) -o prog main.cpp
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "extra.hpp"
+
+
+
+
+int main(int argc, char **argv) {
+
+
+
+  return 0;
+}
